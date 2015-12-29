4a9d7c81ca820b8a3a5d066f6402a3bf5de14a95
Steven Leiva <leiva.steven@gmail.com>
2015-12-28 19:35:22 -0500

    Add the first set of attributes to the serialized version of the rsvp_session.

--- app/serializers/rsvp_session_serializer.rb
+++ app/serializers/rsvp_session_serializer.rb
@@ -1,3 +1,8 @@
 class RsvpSessionSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :rsvp_id, :event_session_id, :created_at, :updated_at,
+             :checked_in, :reminded_at, :role_id
+
+  def role_id
+    object.rsvp.role_id
+  end
 end
